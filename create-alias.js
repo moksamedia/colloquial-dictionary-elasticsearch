60ec8e23eb9f94b22efa31d7867b53c76a2f0d53
Andrew Hughes <andrewcarterhughes@gmail.com>
2024-02-09 18:28:37 +0530

    updated create alias

--- create-alias.js
+++ create-alias.js
@@ -19,6 +19,28 @@ async function run () {
        ],
        name: 'all_colloquial_dicts'
      });
+
+     await client.indices.putAlias({
+         index: [
+           'monlam_tib_eng',
+           'phurba_rangjung_yeshe',
+           'phurba_new_english_tibetan',
+           'phurba_tsultrim_lodro_tib_eng',
+         ],
+         name: 'tib_eng_dicts'
+       });
+
+       await client.indices.putAlias({
+           index: [
+             'monlam_tib_tib',
+             'phurba_mahavyutpatti',
+             'phurba_tshig_mdzod_chen_mo',
+             'phurba_dag_yig_gsar_bsgrigs',
+             'phurba_tsultrim_lodro_tib_tib'
+           ],
+           name: 'tib_tib_dicts'
+         });
+
      console.log("alias all_colloquial_dicts created")
 }
 
